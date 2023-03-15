5f91ec91ce84b414629c2fd0eb8b8cbf6aecab26
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-15 23:04:16 +0100

    feat/add-theme

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        white: "#ffffff",
+        light_gray: "#ebebeb",
+        gray: "#d0d0d0",
+        dark_gray: "#3a4252",
+        light_black: "#252a34",
+        black: "#101216",
+    },
+};

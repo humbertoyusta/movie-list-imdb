1582cfdb617c0d3e21ad3e31c937d8a6655f4d35
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-19 19:19:00 +0100

    fix/movie-styles

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,7 +1,15 @@
 import Movie from "@/components/Movie";
 import useGetMovie from "@/hooks/useGetMovie";
+import Header from "@/components/Header";
 
 export default function MoviePage({ params }: { params: { id: string } }) {
     const movie = useGetMovie({ id: parseInt(params.id) });
-    return movie ? <Movie movie={movie} /> : <></>;
+    return movie ? (
+        <>
+            <Header title={movie.name} />
+            <Movie movie={movie} />
+        </>
+    ) : (
+        <></>
+    );
 }
